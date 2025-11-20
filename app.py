dab1c73a192f48d25468c88b5b296e17cfd7f13e
adep-saham <best.adeprasetyo@gmail.com>
2025-11-20 12:35:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -133,19 +133,11 @@ with st.form("form_input"):
         "Higher is Better", "Lower is Better", "Range"
     ])
 
-   # Input Range Min / Max jika memilih mode Range
-tmin = None
-tmax = None
+  tmin = tmax = None
+    if arah == "Range":
+    tmin = st.number_input("Range Min", 0.0)
+    tmax = st.number_input("Range Max", 0.0)
 
-if arah == "Range":
-    st.markdown("### 🎯 Pengaturan Range Target")
-    cmin, cmax = st.columns(2)
-
-    with cmin:
-        tmin = st.number_input("Target Minimal", value=0.0, step=1.0)
-
-    with cmax:
-        tmax = st.number_input("Target Maksimal", value=0.0, step=1.0)
 
     ket = st.text_area("Keterangan")
 
@@ -369,4 +361,5 @@ if len(df) > 0:
         color_continuous_scale=[COLOR_RED, COLOR_GREY, COLOR_GREEN]
     )
     st.plotly_chart(fig3, use_container_width=True)
+
 
